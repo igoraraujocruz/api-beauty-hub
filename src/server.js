f61d5a54a1e2df7058d6d770292d2f106fc3d1e3
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2023-06-01 21:00:00 -0300

    Update server.js
    AGORA VAI

--- src/server.js
+++ src/server.js
@@ -12,6 +12,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use(
+    cors(),
+);
+
 app.use(routes);
 
 server.listen(port, () => console.log(`Servidor Iniciado na porta ${port}`));
