21f831368054f01810e8eb1baa6c70d172a4629c
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2023-06-15 20:46:55 -0300

    Update Controller.js

--- src/modules/products/infra/Controller.js
+++ src/modules/products/infra/Controller.js
@@ -14,7 +14,7 @@ export class Controller {
     async create(request, response) {
         const { pname, descr, category_id, quantidade, dt_validade } = request.body;
         try {
-            await connection.query(`INSERT INTO products (pname, descr, category_id, dt_validade) VALUES ("${pname}", "${descr}", "${category_id}", "${quantidade}", "${dt_validade}");`);
+            await connection.query(`INSERT INTO products (pname, descr, category_id, quantidade,  dt_validade) VALUES ("${pname}", "${descr}", "${category_id}", "${quantidade}", "${dt_validade}");`);
             return response.status(201).json({ message: 'Criado com sucesso.' });
 
             } catch (error) {
