fddf770f53a67e340edee4c6ce21d96e0cb59c9f
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2023-06-15 21:03:25 -0300

    Update connection.js

--- src/connection.js
+++ src/connection.js
@@ -3,10 +3,10 @@ import { createPool } from 'mysql2/promise';
 console.log("passou no db")
 
 export const connection = createPool({
-  host: '127.0.0.1',
+  host: 'containers-us-west-32.railway.app',
   user: 'root',
-  password: '',
-  database: 'BeautyDB',
-  port: 3306,
+  password: 'xnNLwGVUn263IKAkKvNm',
+  database: 'railway',
+  port: 8025,
   waitForConnections: true,
 });
